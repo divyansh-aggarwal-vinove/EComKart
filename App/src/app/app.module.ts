f1326a5e7a583079b40c879b665379abcac5f8c0
divyansh <61134532+divyansh-aggarwal-vinove@users.noreply.github.com>
2020-05-27 20:24:59 +0530

    Application structure added

---
+++ App/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserRegisterComponent } from './admin/user-register/user-register.component';
+import { ProductDetailsComponent } from './admin/product-details/product-details.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MyorderComponent } from './dashboard/myorder/myorder.component';
+import { CartComponent } from './dashboard/cart/cart.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    UserRegisterComponent,
+    ProductDetailsComponent,
+    DashboardComponent,
+    MyorderComponent,
+    CartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
